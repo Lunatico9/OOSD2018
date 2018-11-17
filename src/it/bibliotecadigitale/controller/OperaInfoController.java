040dd614b546094421d3af5b7d930594da3b731c
Federico Palmitesta <federico.palmitesta@gmail.com>
2018-11-17 13:16:44 +0100

    Add thumbnails to opera info pages
    Note: we need image's absolute path on database, so you need to modify db istance with the specific path to test this part

--- src/it/bibliotecadigitale/controller/OperaInfoController.java
+++ src/it/bibliotecadigitale/controller/OperaInfoController.java
@@ -1,16 +1,30 @@
 package it.bibliotecadigitale.controller;
 
+import java.util.ArrayList;
+
 import it.bibliotecadigitale.dao.OperaDao;
+import it.bibliotecadigitale.dao.PaginaDao;
+
+
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import it.bibliotecadigitale.vo.Datazione;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 
 public class OperaInfoController {
+	
+	//numero di immagini che andiamo a visualizzare in una riga
+	private final int COLUMNS = 6;
 
 	@FXML
 	private Label lblTit;
+	@FXML
+	private ImageView iv;
 	@FXML
 	private Label lblAut;
 	@FXML
@@ -23,16 +37,20 @@ public class OperaInfoController {
 	private Button btnApp;
 	@FXML
 	private Button btnMod;
+	@FXML
+	private GridPane imagePane;
 	
 	/**
 	 * Realizza view personalizzata sulla base dei dati dell'utente
 	 * @throws Exception
 	 */
-	
 	public void defineView() throws Exception {
 		lblTit.setText(Cookie.selectedOpera.getTitolo());
 		lblAut.setText(Cookie.selectedOpera.getAutore());
 		lblCat.setText(Cookie.selectedOpera.getCategoria());
+		
+		ArrayList<String> images = new ArrayList<String>();
+		PaginaDao db = new PaginaDao();
 		
 		if (Cookie.selectedOpera.getDatazione().getAnno() > 1200) {
 			Integer i = new Integer(Cookie.selectedOpera.getDatazione().getAnno());
@@ -54,8 +72,39 @@ public class OperaInfoController {
 		if (Cookie.user.getRuolo() == 'a') {
 			btnMod.setVisible(true);
 		}
+
+		//generiamo le miniature delle pagine dell'opera su j colonne e k righe
 		
-		//Devo generare i thumbnail delle pagine
+		images = db.getImageCollection(Cookie.selectedOpera.getId());
+		
+		int k = 0; int j = 0;
+		if (!images.isEmpty()) {
+			for(String i : images) {
+				
+			    Image image = new Image(i);
+			    iv.setImage(image);
+				iv.setFitWidth(100);
+				iv.setFitHeight(200);
+		        iv.setPreserveRatio(true);
+		        iv.setSmooth(true);
+		        iv.setCache(true);
+				iv.setOnMouseClicked(new EventHandler<MouseEvent>() {
+					@Override
+	                public void handle(MouseEvent event) {
+						System.out.println("clicked");
+						//Main.toViewer(event);
+					}
+				});
+				
+				if (j < COLUMNS) {
+					imagePane.add(iv, j++, k);
+				}
+				else {
+					imagePane.add(iv, j++, k++);
+				}
+				
+			}
+		}
 	}
 	
 	/**
@@ -88,7 +137,7 @@ public class OperaInfoController {
 	 * @param ActionEvent event
 	 */
 	public void modOpera(ActionEvent event) {
-		//Main.toModOpera(event);
+		Main.toModOpera(event);
 	}
 	
 }
