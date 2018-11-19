a3b6979a36b2e9ce753fda66799ba88f48a22fc4
Federico Palmitesta <federico.palmitesta@gmail.com>
2018-11-19 18:43:24 +0100

    Add assign opera to transcriber

--- src/it/bibliotecadigitale/controller/OperaInfoController.java
+++ src/it/bibliotecadigitale/controller/OperaInfoController.java
@@ -4,15 +4,20 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import org.controlsfx.control.textfield.TextFields;
+
 import it.bibliotecadigitale.model.dao.OperaDao;
 import it.bibliotecadigitale.model.dao.PaginaDao;
+import it.bibliotecadigitale.model.dao.UtenteDao;
 import it.bibliotecadigitale.model.Pagina;
+import it.bibliotecadigitale.model.Utente;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -20,6 +25,8 @@ import javafx.scene.layout.TilePane;
 
 public class OperaInfoController implements Initializable{
 
+	@FXML
+	private Label lblErr;
 	@FXML
 	private Label lblTit;
 	@FXML
@@ -36,6 +43,10 @@ public class OperaInfoController implements Initializable{
 	private Button btnMod;
 	@FXML
 	private Button btnDel;
+	@FXML
+	private Button btnAss;
+	@FXML
+	private TextField txtSearch;
 	@FXML
 	private TilePane imagePane;
 	
@@ -164,6 +175,75 @@ public class OperaInfoController implements Initializable{
 		Cookie.selectedOpera = null;
 		
 		Main.toSearchOpera(event);
+	}
+	
+	/**
+	 * Implementa autocompletamento sulla barra di ricerca
+	 */
+	public void autoCompletion () {
+		String input = txtSearch.getText();
+		
+		ArrayList<String> usernames = new ArrayList<String>();
+		ArrayList<Utente> users = new ArrayList<Utente>();
+		
+		UtenteDao db = new UtenteDao();
+		
+		users.clear();
+		usernames.clear();
+		
+		try {
+			users = db.searchUserByLogin(input, "Trascrittore");
+			
+			if(!users.isEmpty()) {
+				for(Utente u : users) {
+					usernames.add(u.getLogin());
+				}
+				TextFields.bindAutoCompletion(txtSearch, usernames);
+			}
+		}
+		catch (Exception e) {
+			Main.toErrorMsg("Errore in connessione al Database");
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Assegna l'opera al trascrittore selezionato
+	 */
+	public void assign() {
+		UtenteDao db = new UtenteDao();
+		
+		String input = txtSearch.getText();
+		lblErr.setText("");
+		
+		try {
+			if (input == null) {
+				lblErr.setText("Inserisci il nome utente di un trascrittore");
+			}
+			
+			else if (db.isNotRegistered(input)) {
+				lblErr.setText("L'utente cercato non esiste");
+			}
+			
+			else {
+				Utente user = new Utente();
+				user = db.getUtente(input);
+				if (user.getRuolo() != 't') {
+					lblErr.setText("L'utente cercato non è un trascrittore");
+				}
+				else {
+					OperaDao od = new OperaDao();
+					od.allocateOpera(user.getId(), Cookie.selectedOpera.getId());
+				}
+				
+			}
+		
+		}
+		catch (Exception e){
+			
+		}
+		
+			
 	}
 	
 }
