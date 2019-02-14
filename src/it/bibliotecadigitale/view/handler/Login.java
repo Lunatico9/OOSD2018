63a22c6fd43c6b50788995b87411c330cafc6b1f
Mirko Pizii <hallino1@gmail.com>
2019-02-14 17:17:03 +0100

    Added comments and first input focus for Login

--- src/it/bibliotecadigitale/view/handler/Login.java
+++ src/it/bibliotecadigitale/view/handler/Login.java
@@ -1,14 +1,19 @@
 package it.bibliotecadigitale.view.handler;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import it.bibliotecadigitale.controller.LoginController;
 import it.bibliotecadigitale.controller.Main;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class Login {
+public class Login implements Initializable {
 		
 	@FXML
 	private Label lblStatus;
@@ -20,6 +25,25 @@ public class Login {
 	private TextField txtPassword;
 
 
+	/**
+	 * Procedura di inizializzazione
+	 * 
+	 * @param location viene usato per risolvere percorsi relativi all'oggetto root
+	 * o restituisce null se la variabile non è conosciuta
+     *
+     * @param resources viene usato per localizzare l'oggetto root o restituisce
+     * null se l'oggetto root non viene localizzato.
+	 */
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		Platform.runLater(new Runnable() {
+			@Override
+			public void run() {
+				txtUsername.requestFocus();
+			}
+		});		
+	}
+	
 	/**
 	 * Procedura di login
 	 * 
